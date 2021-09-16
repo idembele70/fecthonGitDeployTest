3b4e5a4abd0242d5c97f92e9a9e9a86a0afaf865
idembele70 <idembele70@gmail.com>
2021-09-16 14:25:07 +0200

    [App] initilized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(
+    () => {
+      fetch("data.json")
+      .then(
+        res => res.json()
+      )
+      .then(setData)
+      .catch(e => console.error("Error while fetching Todo in App file", e))
+    }
+    , []
+  )
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        DATA
       </header>
+      <div>
+        {data.length && data.map(
+          (name, idx) => <p key={idx}>{name.firstname}</p>
+        )}
+      </div>
     </div>
   );
 }
